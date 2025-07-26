55d91cf0e369bfb834dbca9bae7bd9b22864302f
Wallace027Dev <wallaceofc@hotmail.com>
2025-07-26 17:27:07 -0300

    refatorando pagina de inscricao

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import { type InputHTMLAttributes, forwardRef } from "react";
+import { forwardRef, type InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  $hasError?: boolean;
+  hasError?: boolean;
   errorMessage?: string;
 }
 
@@ -10,7 +10,7 @@ const StyledInput = styled.input<{ $hasError?: boolean }>`
   height: 3rem;
   width: 100%;
   min-width: 17.5rem;
-  color: #130f0b;
+  color: rgba(23, 26, 31, 0.7);
   background: #d0ced1;
   font-size: 1rem;
   border-radius: 0.75rem;
@@ -30,15 +30,6 @@ const StyledInput = styled.input<{ $hasError?: boolean }>`
     box-shadow: 6px 8px 6px 0 rgba(122, 47, 25, 0.3);
   }
 
-  &:-webkit-autofill {
-    -webkit-text-fill-color: #000 !important;
-    transition: background-color 5000s ease-in-out 0s;
-  }
-
-  &::placeholder {
-    color: rgba(66, 89, 42, 0.5);
-  }
-
   & + & {
     margin-top: 0.5rem;
   }
@@ -51,11 +42,11 @@ const ErrorMessage = styled.p`
 `;
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ $hasError, errorMessage, ...props }, ref) => (
-    <>
-      <StyledInput ref={ref} $hasError={$hasError} {...props} />
-      {$hasError && errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-    </>
+  ({ hasError, errorMessage, ...props }, ref) => (
+    <div style={{ width: "100%" }}>
+      <StyledInput ref={ref} $hasError={hasError} {...props} />
+      {hasError && errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </div>
   )
 );
 
---
+++ src/components/ui/Form.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: end;
+
+  span {
+    color: #42592a;
+    margin-top: 0.5rem;
+  }
+`;
